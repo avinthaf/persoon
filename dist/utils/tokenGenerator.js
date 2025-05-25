0ed549f1f7756c633eefd1c9d3235179e2426ca8
Avintha Fonseka <fonsekaavintha@gmail.com>
2025-05-25 13:47:45 -0400

    Remove build artifacts from repository

--- dist/utils/tokenGenerator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateBearerToken = generateBearerToken;
-const crypto_1 = require("crypto");
-function generateBearerToken(length = 32) {
-    return (0, crypto_1.randomBytes)(length).toString('hex');
-}
